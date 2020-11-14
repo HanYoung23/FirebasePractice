b8c6ca625621dd4203ef8b23a986dd176dd6f224
hanyoung23 <67942048+HanYoung23@users.noreply.github.com>
2020-11-15 00:35:11 +0900

    login form part

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
 
 const AppRouter = (isLoggedIn) => {
+  console.log(isLoggedIn);
   return (
     <Router>
       <Switch>
-        {isLoggedIn ? (
+        {isLoggedIn === null ? (
           <>
             <Route exact path="/">
               <Home></Home>
