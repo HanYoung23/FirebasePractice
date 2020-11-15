e61eb0c903c3cb492ee8d75dc34a03edce88f80b
hanyoung23 <67942048+HanYoung23@users.noreply.github.com>
2020-11-15 23:22:07 +0900

    log out:

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,21 +1,36 @@
 import React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom";
 import Auth from "routes/Auth";
 import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "./Navigation";
 
 const AppRouter = (isLoggedIn) => {
   const login = isLoggedIn.isLoggedIn;
   return (
     <Router>
+      {login && <Navigation />}
       <Switch>
         {login ? (
-          <Route exact path="/">
-            <Home></Home>
-          </Route>
+          <>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/profile">
+              <Profile />
+            </Route>
+          </>
         ) : (
-          <Route exact path="/">
-            <Auth />
-          </Route>
+          <>
+            <Route exact path="/">
+              <Auth />
+            </Route>
+          </>
         )}
       </Switch>
     </Router>
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,5 +1,19 @@
+import { authService } from "fbase";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
-const Profile = () => <span>Profile</span>;
+const onLogOutClick = () => {
+  authService.signOut();
+  let history = useHistory();
+  history.push("/");
+};
+
+const Profile = () => {
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
 
 export default Profile;
