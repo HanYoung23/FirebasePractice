972f0c6707a9e3eebc0999905dab262f202980ed
hanyoung23 <67942048+HanYoung23@users.noreply.github.com>
2020-11-16 23:25:22 +0900

    form and database

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -2,16 +2,22 @@ import { authService } from "fbase";
 import React from "react";
 import { useHistory } from "react-router-dom";
 
-const onLogOutClick = () => {
-  authService.signOut();
-  let history = useHistory();
-  history.push("/");
-};
+// const onLogOutClick = () => {
+//   authService.signOut();
+// };
 
 const Profile = () => {
+  const history = useHistory();
   return (
     <>
-      <button onClick={onLogOutClick}>Log Out</button>
+      <button
+        onClick={() => {
+          history.push("/");
+          authService.signOut();
+        }}
+      >
+        Log Out
+      </button>
     </>
   );
 };
